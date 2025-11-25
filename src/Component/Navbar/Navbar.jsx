27eb4c30e77b8c810b73c746016b7110cb2130dc
rajalishikhe <nahidujjamanraj@gmail.com>
2025-11-25 09:21:49 +0600

    Now I am complete my Register part Design Complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <li><NavLink to={"/Register"}>Register page</NavLink></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Simple Login Page </a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -27,7 +27,7 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">User Name </a>
   </div>
 </div>
         </div>
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -3,7 +3,25 @@ const Register = () => {
     return (
         <div>
             <h2>Welcome to the Register page </h2>
-            
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-2">
+      <div className="card-body">
+        <fieldset className="fieldset">
+         
+          <label className="label">Name</label>
+          <input name="name" type="name" className="input" placeholder="Name" />
+           <label className="label">Email</label>
+           <input name="email" type="email" className="input" placeholder="Email" /> 
+            <label className="label">Phone Number </label>
+           <input name="phone_number" type="number" className="input" placeholder="Phone_Number"/>
+           <label className="label">Password</label>
+           <input type="password" className="input" placeholder="Password" />
+           <label className="label">Country Name </label>
+           <input type="text" className="input" placeholder="Country name" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </div>
+    </div>
         </div>
     );
 };
