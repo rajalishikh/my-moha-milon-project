fbb49d5752392850061792c5eac90543967eab01
rajalishikhe <nahidujjamanraj@gmail.com>
2025-11-25 09:52:42 +0600

    Now I am reacive The value of input field

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,17 +1,29 @@
+import { Link } from "react-router";
 
 const Login = () => {
+    const handleLogin=(e)=>{
+        e.preventDefault()
+        console.log("welcome to the login page")
+        const name =e.target.name.value;
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        console.log(name,email,password)
+
+    }
     return (
         <div>
 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-2">
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form  onSubmit={handleLogin} className="fieldset">
+            <label className="label">Name</label>
+          <input name="name" type="text" className="input" placeholder="Email" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input name="email" type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
+          <input name="password" type="password" className="input" placeholder="Password" />
+          <div className=" font-medium text-green-500"> You are new User ? <Link to={'/Register'} ><span className="link link-hover text-red-500">Register</span></Link></div>
           <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        </form>
       </div>
     </div>
   
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 const Register = () => {
     return (
@@ -5,21 +6,21 @@ const Register = () => {
             <h2>Welcome to the Register page </h2>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-2">
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form   className="fieldset">
          
           <label className="label">Name</label>
           <input name="name" type="name" className="input" placeholder="Name" />
            <label className="label">Email</label>
            <input name="email" type="email" className="input" placeholder="Email" /> 
-            <label className="label">Phone Number </label>
-           <input name="phone_number" type="number" className="input" placeholder="Phone_Number"/>
+           <label className="label">Phone Number </label>
+           <input name="phone_number" type="tel" className="input" placeholder="Phone_Number"/>
            <label className="label">Password</label>
            <input type="password" className="input" placeholder="Password" />
            <label className="label">Country Name </label>
            <input type="text" className="input" placeholder="Country name" />
-          <div><a className="link link-hover">Forgot password?</a></div>
+          <div className=" font-medium text-green-500"> You have All ready account ? <Link to={'/login'} ><span className="link link-hover text-red-500">Login</span></Link></div>
           <button className="btn btn-neutral mt-4">Register</button>
-        </fieldset>
+        </form>
       </div>
     </div>
         </div>
