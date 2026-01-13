6555d723b4476afa2bbd4375d8b335b0675f7f67
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-01-13 12:19:06 +0600

    just make ordersList

--- src/Component/Orders/Orders.jsx
+++ src/Component/Orders/Orders.jsx
@@ -3,6 +3,14 @@ const Orders = () => {
     return (
         <div>
             <h2>Here is Your Orders Page </h2>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
             
         </div>
     );
