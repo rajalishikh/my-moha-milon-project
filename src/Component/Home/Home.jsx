177cfae3dbd17225ff399d5a72e95989db9914a6
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-01-20 13:06:57 +0600

    Just set MY target

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
     return (
         <div>
             <h2>Welcome to the Home page </h2>
+            <p>Now I am start my Last journey to be a programmer IF I am fail I am decide that I will be I will leave this side foreEver </p>
             
         </div>
     );
