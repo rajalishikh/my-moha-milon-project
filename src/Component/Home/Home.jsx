a1123a2a3812696bd1899d87b8ac8c13146b7443
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-01-27 08:42:21 +0600

    Now I am understand my work

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
         <div>
             <h2>Welcome to the Home page </h2>
             <p>Now I am start my Last journey to be a programmer IF I am fail I am decide that I will be I will leave this side foreEver </p>
+            <h2>Now I am Understand my code </h2>
             
         </div>
     );
